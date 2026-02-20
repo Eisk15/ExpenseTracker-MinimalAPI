ece754871229c498d509cb032297c6d9fbd30442
Kyle Esurena <kyleesurena1@gmail.com>
2026-02-20 20:55:54 +0800

    Postgresql Connected

--- ExpenseTrackerMinimalAPI/Program.cs
+++ ExpenseTrackerMinimalAPI/Program.cs
@@ -1,8 +1,11 @@
 using Scalar.AspNetCore;
+using ExpenseTrackerMinimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
