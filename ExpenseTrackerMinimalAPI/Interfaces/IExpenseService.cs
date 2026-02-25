2ce66451e2ddb8692c9c92684b5be57cb14f4201
Kyle Esurena <kyleesurena1@gmail.com>
2026-02-25 19:54:33 +0800

    Added interfaces

---
+++ ExpenseTrackerMinimalAPI/Interfaces/IExpenseService.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTrackerMinimalAPI.Models;
+namespace ExpenseTrackerMinimalAPI.Interfaces
+{
+    public interface IExpenseService
+    {
+        Expense GetExpenseById(int id);
+        List<Expense> GetExpenses();
+        List<Expense> GetExpenseByUser(int userId);
+        Expense CreateExpense(Expense expense);
+        Expense UpdateExpense(Expense expense);
+        bool DeleteExpense(int id);
+    }
+}
---
+++ ExpenseTrackerMinimalAPI/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseTrackerMinimalAPI.Models;
+namespace ExpenseTrackerMinimalAPI.Interfaces
+{
+    public interface IUserService
+    {
+        User CreateUser(User user);
+        bool DeleteUser(int id);
+        User GetUserById(int id);
+        User GetUserByUsername(string username);
+        bool UpdatePassword(int id, string newPassword);
+    }
+}
