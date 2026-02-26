d32af89f56c10dd1b170cf4c17ccda837ab408f6
Kyle Esurena <kyleesurena1@gmail.com>
2026-02-26 18:41:33 +0800

    Added user service

--- ExpenseTrackerMinimalAPI/Interfaces/IUserService.cs
+++ ExpenseTrackerMinimalAPI/Interfaces/IUserService.cs
@@ -3,10 +3,10 @@ namespace ExpenseTrackerMinimalAPI.Interfaces
 {
     public interface IUserService
     {
-        User CreateUser(User user);
-        bool DeleteUser(int id);
-        User GetUserById(int id);
-        User GetUserByUsername(string username);
-        bool UpdatePassword(int id, string newPassword);
+        Task<User> CreateUser(User user);
+        Task<bool> DeleteUser(int id);
+        Task<User?> GetUserById(int id);
+        Task<User?> GetUserByUsername(string username);
+        Task<bool> UpdatePassword(int id, string newPassword);
     }
 }
---
+++ ExpenseTrackerMinimalAPI/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using ExpenseTrackerMinimalAPI.Models;
+using ExpenseTrackerMinimalAPI.Interfaces;
+using ExpenseTrackerMinimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+namespace ExpenseTrackerMinimalAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+
+        // Constructor
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Methods
+
+        public async Task<User> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteUser(int id)
+        {
+            // Find user
+            var user = await _context.Users.FindAsync(id);
+            if (user==null) { return false; }
+
+            // Delete user
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<User?> GetUserById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User?> GetUserByUsername(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<bool> UpdatePassword(int id, string newPassword)
+        {
+            // Find user
+            var user = await _context.Users.FindAsync(id);
+            if(user == null) { return false; }
+
+            // Change pass
+            user.PasswordHash = newPassword;
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
