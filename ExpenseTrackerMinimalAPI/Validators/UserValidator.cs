b2f95b131da92cee831725218aebafed1366bb0b
Kyle Esurena <kyleesurena1@gmail.com>
2026-03-04 10:46:00 +0800

    added validators using FluentValidation

---
+++ ExpenseTrackerMinimalAPI/Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ExpenseTrackerMinimalAPI.Models;
+namespace ExpenseTrackerMinimalAPI.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Username).NotEmpty().WithMessage("Username is required!")
+                .MinimumLength(5).WithMessage("Minimum length is 5!")
+                .MaximumLength(20).WithMessage("Maximum Length is 20!");
+            RuleFor(u => u.PasswordHash).NotEmpty().WithMessage("Password is required!")
+                .MinimumLength(8).WithMessage("Minimum password length is 8");
+        }
+    }
+}
